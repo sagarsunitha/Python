f01dc559933e94d266672b63a8401fa9a4a5fa25
sagarsunitha <saisunitha23@gmail.com>
2021-04-16 23:23:03 +0100

    Committed by Sunitha
    Python exercises  until data types.

---
+++ Python Exercise-Copy1 (1).py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+
+# In[6]:
+
+
+# This is a comment
+print("Hello World")
+
+
+# In[7]:
+
+
+"""
+This is a comment
+written in 
+more that just one line
+"""
+print("Hello World")
+
+
+# In[8]:
+
+
+x=50
+print ("x")
+
+
+# In[9]:
+
+
+x=5
+y=10
+print(x+y)
+
+
+# In[10]:
+
+
+x=5;y=10;
+z=x+y
+print(z)
+
+
+# In[11]:
+
+
+myfirst_name = "John"
+#remove illegal char
+
+
+# In[12]:
+
+
+x = 5
+print(type(x))
+
+
+# In[13]:
+
+
+x = "Hello World"
+print(type(x))
+
+
+# In[14]:
+
+
+x = 20.5
+print(type(x))
+
+
+# In[15]:
+
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+
+# In[16]:
+
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+
+# In[17]:
+
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+
+# In[18]:
+
+
+x = True
+print(type(x))
+
+
+# In[20]:
+
+
+x = 5
+x =float(x)
+print(x)
+
+
+# In[22]:
+
+
+x = 5.5
+x = int(x)
+print(x)
+
+
+# In[23]:
+
+
+x = 5
+x = complex(x)
+print(x)
+
+
+# In[1]:
+
+
+x = "Sunitha World"
+print(len(x))
+
+
+# In[27]:
+
+
+txt = "Hello World"
+x = txt[0]
+print(x)
+
+
+# In[28]:
+
+
+txt = "Hello World"
+x = txt[2:5]
+print(x)
+
+
+# In[2]:
+
+
+txt = " Test World "
+x = txt.strip()
+print(x)
+
+
+# In[30]:
+
+
+txt = "Hello World"
+txt = txt.upper()
+print(txt)
+
+
+# In[31]:
+
+
+txt = "Hello World"
+txt = txt.lower()
+print(txt)
+
+
+# In[32]:
+
+
+txt = "Hello World"
+txt = txt.replace("H", "J")
+print(txt)
+
+
+# In[33]:
+
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+
+# In[ ]:
+
+
+
+
